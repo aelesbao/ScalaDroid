e0baee653f1a3972253495fd683ac43b6ecfc81c
Augusto Rocha Elesbão <aelesbao@gmail.com.br>
2012-02-22 03:56:04 -0200

    adding scala code :)

---
+++ src/com/github/aelesbao/scaladroid/MainActivity.scala
@@ -0,0 +1,11 @@
+package com.github.aelesbao.scaladroid
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) : Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+  }
+}
